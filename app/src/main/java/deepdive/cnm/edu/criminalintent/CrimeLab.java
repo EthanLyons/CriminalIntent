98b90e07937877dbd9347cc222b87b350880f5cc
Ethan Lyons <ethan.lyons84@gmail.com>
2019-03-06 08:04:32 -0700

    Started building fragments(unsuccesfully) :/

--- app/src/main/java/deepdive/cnm/edu/criminalintent/CrimeLab.java
+++ app/src/main/java/deepdive/cnm/edu/criminalintent/CrimeLab.java
@@ -1,19 +1,44 @@
 package deepdive.cnm.edu.criminalintent;
 
 import android.content.Context;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class CrimeLab {
   private static CrimeLab sCrimelab;
 
-  public static Crimelab get(Context context) {
+  private List<Crime> mCrimes;
+
+  public static CrimeLab get(Context context) {
     if (sCrimelab == null)  {
-      sCrimelab = new Crimelab(context);
+      sCrimelab = new CrimeLab(context);
     }
     return sCrimelab;
   }
 
   private CrimeLab(Context context) {
+    mCrimes = new ArrayList<>();
+    for (int i = 0; i < 100; i++) {
+      Crime crime = new Crime();
+      crime.setTitle("Crime #" + i);
+      crime.setSolved(i % 2 == 0);
+
+      mCrimes.add(crime);
+
+    }
+  }
+
+  public Crime getCrime(UUID id)  {
+    for (Crime crime: mCrimes)  {
+      if (crime.getId().equals(id))  {
+        return crime;
+      }
+    }
+
+    return null;
 
   }
+
 
 }
